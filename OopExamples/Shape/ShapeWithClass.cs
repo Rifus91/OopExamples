57ec06558570bf4d408160dfb530233aad463b74
Rif <Rifus91@mail.ru>
2021-03-30 20:34:05 +0300

    добавил Shape - пример наследования. заполнил на 50%

---
+++ OopExamples/Shape/ShapeWithClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopExamples.ShapeClass
+{
+    public abstract class Shape
+    {
+        public abstract double Area();
+        public abstract void Draw();
+        public virtual void Fill()
+        { Console.WriteLine("Заливка фигуры цветом"); }
+    }
+
+    public class Circle: Shape 
+    { 
+        private double r;
+        private readonly double pi = 3.14;
+
+        public Circle(double r)
+        {
+            this.r = r;
+        }
+        public override double Area()
+        {
+            return pi * r * r;
+        }
+        public override void Draw()
+        {
+            Console.WriteLine($"Рисует круг радиусом {r}");
+        }
+    }
+
+}
